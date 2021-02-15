e8e248a1df3c892f85d4e15f882bf485a11b0e52
Caitlin Hemlock <hemlock_caitlin@berkeley.edu>
2021-02-15 11:51:58 -0800

    kenya data cleaning

--- Kenya/1-data cleaning-kenya.R
+++ Kenya/1-data cleaning-kenya.R
@@ -14,3 +14,10 @@ library(SmartEDA)
 library(ggpubr)
 library(dplyr)
 library(ggplot2)
+
+#read in dataset
+d <- read.csv(file = paste0(dropboxDir, "Data/Cleaned/Caitlin/kenya-dm-ee-anthro-ee.csv"))
+
+colnames(d)
+stool <- read.csv(file = paste0(dropboxDir, "Data/Cleaned/Andrew/washb-kenya-eed-stool.csv"))
+d <- left_join(d, wealth, by = c("dataid", "clusterid", "block"))
