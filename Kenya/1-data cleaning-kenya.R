0b191bdd3226399f5a7d406c227d93c0b39f7397
Caitlin Hemlock <hemlock_caitlin@berkeley.edu>
2021-02-15 11:49:25 -0800

    move to Kenya folder

---
+++ Kenya/1-data cleaning-kenya.R
@@ -0,0 +1,16 @@
+rm(list=ls())
+source(here::here("0-config.R"))
+
+#install packages as needed
+#install.packages("forcats")
+#install.packages("SmartEDA")
+#install.packages("ggpubr")
+#install.packages("dplyr")
+#install.packages("ggplot2")
+
+#load libraries
+library(forcats)
+library(SmartEDA)
+library(ggpubr)
+library(dplyr)
+library(ggplot2)
