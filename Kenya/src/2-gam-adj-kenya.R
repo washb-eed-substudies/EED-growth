9f5621d766021c3889304ff0dd4276f303ad8ea7
Caitlin Hemlock <hemlock_caitlin@berkeley.edu>
2021-03-07 11:03:00 -0800

    Kenya adjusted analysis

---
+++ Kenya/src/2-gam-adj-kenya.R
@@ -0,0 +1,156 @@
+rm(list=ls())
+
+source(here::here("Kenya/1-data cleaning-Kenya.R"))
+#remotes::install_github('washb-eed-substudies/washbgam', force = TRUE)
+library(washbgam)
+
+#Make vectors of adjustment variable names
+covariates <- read.csv(file = paste0(dropboxDir, "WBK-EE-analysis/Data/Cleaned/Caitlin/EED-Growth Covariates - Kenya.csv"))
+
+#baseline covariates
+w.vars <- covariates[c(44:56),1]
+w.vars <- w.vars[-10] #remove floor which has no variation
+
+#timevarying covariates
+time.cov <- covariates[c(1:40),c(2,8:18)]
+
+#obtain unique adjustment sets
+time.cov <- unique(time.cov)
+
+#make blank cells NAs
+time.cov <- time.cov %>% na_if("")
+
+#create adjustment set vectors
+cov.list <- vector('list', nrow(time.cov))
+for(i in 1:length(cov.list)){
+  vec <- as.character(time.cov[i,2:12]) # create vector using row i in columns with variable names
+  vec <- append(vec, w.vars) #add baseline characteristics
+  vec <- vec[!is.na(vec)] #remove any NAs (from blank cells)
+  cov.list[[i]] <- vec #insert into list item i
+}
+names(cov.list) <- paste0('adjset', seq_along(cov.list)) #rename 
+
+urine.t1 <- c("ln_lact1", "ln_mann1")
+stool.t1 <- c("ln_mpo1", "ln_aat1", "ln_neo1")
+urine.t2 <- c("ln_lact2", "ln_mann2")
+stool.t2 <- c("ln_mpo2", "ln_aat2", "ln_neo2")
+urine.t3 <- c("ln_lact3", "ln_mann3")
+stool.t3 <- c("ln_mpo3", "ln_aat3", "ln_neo3")
+
+all.growth.t1 <- c("laz_t1", "waz_t1", "hcz_t1", "whz_t1")
+laz.waz.t1 <- c("laz_t1", "waz_t1")
+hcz.t1 <- "hcz_t1"
+whz.t1 <- "whz_t1"
+
+laz.waz.t2 <- c("laz_t2", "waz_t2")
+hcz.t2 <- "hcz_t2"
+whz.t2 <- "whz_t2"
+
+laz.waz.t3 <- c("laz_t3", "waz_t3")
+hcz.t3 <- "hcz_t3"
+whz.t3 <- "whz_t3"
+
+velo.t1.t2 <- c("len_velocity_t1_t2", "wei_velocity_t1_t2", "hc_velocity_t1_t2")
+velo.t1.t3 <- c("len_velocity_t1_t3", "wei_velocity_t1_t3", "hc_velocity_t1_t3")
+velo.t2.t3 <- c("len_velocity_t2_t3", "wei_velocity_t2_t3", "hc_velocity_t2_t3")
+
+###### Analysis
+gam.analysis <- function(Xvar = NULL, Yvar = NULL, data = d, Wvar = NULL){
+  for(i in Xvar){
+    for(j in Yvar){
+      print(i)
+      print(j)
+      res_adj <- fit_RE_gam(d=data, X=i, Y=j,  W=Wvar)
+      res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
+      all_models <- bind_rows(all_models, res)
+    }
+  }
+  return(all_models)
+}
+
+all_models <- NULL
+#analysis numbers are from covariates spreadsheet
+all_models <- gam.analysis(Xvar = stool.t1, Yvar = all.growth.t1, W = cov.list[["adjset1"]]) #1
+all_models <- gam.analysis(Xvar = urine.t1, Yvar = all.growth.t1, W = cov.list[["adjset2"]]) #2
+all_models <- gam.analysis(Xvar = stool.t2, Yvar = laz.waz.t2, W = cov.list[["adjset3"]]) #3
+all_models <- gam.analysis(Xvar = urine.t2, Yvar = laz.waz.t2, W = cov.list[["adjset4"]]) #4
+all_models <- gam.analysis(Xvar = stool.t2, Yvar = hcz.t2, W = cov.list[["adjset5"]]) #5
+all_models <- gam.analysis(Xvar = urine.t2, Yvar = hcz.t2, W = cov.list[["adjset6"]]) #6
+all_models <- gam.analysis(Xvar = stool.t2, Yvar = whz.t2, W = cov.list[["adjset7"]]) #7
+all_models <- gam.analysis(Xvar = urine.t2, Yvar = whz.t2, W = cov.list[["adjset8"]]) #8
+all_models <- gam.analysis(Xvar = stool.t3, Yvar = laz.waz.t3, W = cov.list[["adjset9"]]) #9
+all_models <- gam.analysis(Xvar = urine.t3, Yvar = laz.waz.t3, W = cov.list[["adjset10"]]) #10
+all_models <- gam.analysis(Xvar = stool.t3, Yvar = hcz.t3, W = cov.list[["adjset11"]]) #11
+all_models <- gam.analysis(Xvar = urine.t3, Yvar = hcz.t3, W = cov.list[["adjset12"]]) #12
+all_models <- gam.analysis(Xvar = stool.t3, Yvar = whz.t3, W = cov.list[["adjset13"]]) #13
+all_models <- gam.analysis(Xvar = urine.t3, Yvar = whz.t3, W = cov.list[["adjset14"]]) #14
+all_models <- gam.analysis(Xvar = stool.t1, Yvar = laz.waz.t2, W = cov.list[["adjset15"]]) #15
+all_models <- gam.analysis(Xvar = urine.t1, Yvar = laz.waz.t2, W = cov.list[["adjset16"]]) #16
+all_models <- gam.analysis(Xvar = stool.t1, Yvar = hcz.t2, W = cov.list[["adjset17"]]) #17
+all_models <- gam.analysis(Xvar = urine.t1, Yvar = hcz.t2, W = cov.list[["adjset18"]]) #18
+all_models <- gam.analysis(Xvar = stool.t1, Yvar = whz.t2, W = cov.list[["adjset19"]]) #19
+all_models <- gam.analysis(Xvar = urine.t1, Yvar = whz.t2, W = cov.list[["adjset20"]]) #20
+all_models <- gam.analysis(Xvar = stool.t1, Yvar = laz.waz.t3, W = cov.list[["adjset21"]]) #21
+all_models <- gam.analysis(Xvar = urine.t1, Yvar = laz.waz.t3, W = cov.list[["adjset22"]]) #22
+all_models <- gam.analysis(Xvar = stool.t1, Yvar = hcz.t3, W = cov.list[["adjset23"]]) #23
+all_models <- gam.analysis(Xvar = urine.t1, Yvar = hcz.t3, W = cov.list[["adjset24"]]) #24
+all_models <- gam.analysis(Xvar = stool.t1, Yvar = whz.t3, W = cov.list[["adjset25"]]) #25
+all_models <- gam.analysis(Xvar = urine.t1, Yvar = whz.t3, W = cov.list[["adjset26"]]) #26
+all_models <- gam.analysis(Xvar = stool.t2, Yvar = laz.waz.t3, W = cov.list[["adjset27"]]) #27
+all_models <- gam.analysis(Xvar = urine.t2, Yvar = laz.waz.t3, W = cov.list[["adjset28"]]) #28
+all_models <- gam.analysis(Xvar = stool.t2, Yvar = hcz.t3, W = cov.list[["adjset29"]]) #29
+all_models <- gam.analysis(Xvar = urine.t2, Yvar = hcz.t3, W = cov.list[["adjset30"]]) #30
+all_models <- gam.analysis(Xvar = stool.t2, Yvar = whz.t3, W = cov.list[["adjset31"]]) #31
+all_models <- gam.analysis(Xvar = urine.t2, Yvar = whz.t3, W = cov.list[["adjset32"]]) #32
+all_models <- gam.analysis(Xvar = stool.t1, Yvar = velo.t1.t2, W = cov.list[["adjset19"]]) #33
+all_models <- gam.analysis(Xvar = urine.t1, Yvar = velo.t1.t2, W = cov.list[["adjset20"]]) #34
+all_models <- gam.analysis(Xvar = stool.t1, Yvar = velo.t1.t3, W = cov.list[["adjset25"]]) #35
+all_models <- gam.analysis(Xvar = urine.t1, Yvar = velo.t1.t3, W = cov.list[["adjset26"]]) #36
+all_models <- gam.analysis(Xvar = stool.t1, Yvar = velo.t2.t3, W = cov.list[["adjset33"]]) #37
+all_models <- gam.analysis(Xvar = urine.t1, Yvar = velo.t2.t3, W = cov.list[["adjset34"]]) #38
+all_models <- gam.analysis(Xvar = stool.t2, Yvar = velo.t2.t3, W = cov.list[["adjset31"]]) #39
+all_models <- gam.analysis(Xvar = urine.t2, Yvar = velo.t2.t3, W = cov.list[["adjset32"]]) #40
+
+
+#bind all together
+all_models_res <- NULL
+for(i in 1:nrow(all_models)){
+  res <- data.frame(X=all_models$X[i], Y=all_models$Y[i])
+  preds <- predict_gam_diff(fit=all_models$fit[i][[1]], d=all_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  gamm_diff_res <- preds$res
+  all_models_res <-  bind_rows(all_models_res, gamm_diff_res)
+}
+
+#BH procedure
+all.splits <- list(NA)
+all.splits[[1]] <- all_models_res[1:20,]
+all.splits[[2]] <- all_models_res[21:40,]
+all.splits[[3]] <- all_models_res[41:60,]
+all.splits[[4]] <- all_models_res[61:100,]
+all.splits[[5]] <- all_models_res[101:120,]
+all.splits[[6]] <- all_models_res[121:165,]
+all.splits[[7]] <- all_models_res[166:nrow(all_models_res),]
+
+total <- NA
+for (i in 1:length(all.splits)) {
+  all.splits[[i]] <- all.splits[[i]] %>%
+    mutate(corrected.Pval=p.adjust(Pval, method="BH")) %>%
+    as.data.frame()
+  
+  total <- rbind(total, all.splits[[i]])
+}
+total <- total[-1,]
+
+H1_adj_res_BH <- total[1:60,]
+H2_adj_res_BH <- total[61:100,]
+H3_adj_res_BH <- total[101:120,]
+H4_adj_res_BH <- total[121:165,]
+H5_adj_res_BH <- total[166:nrow(total),]
+
+
+saveRDS(total, here("Kenya/results/adjusted/all_results.RDS"))
+saveRDS(H1_adj_res_BH, here("Kenya/results/adjusted/H1_res.RDS"))
+saveRDS(H2_adj_res_BH, here("Kenya/results/adjusted/H2_res.RDS"))
+saveRDS(H3_adj_res_BH, here("Kenya/results/adjusted/H3_res.RDS"))
+saveRDS(H4_adj_res_BH, here("Kenya/results/adjusted/H4_res.RDS"))
+saveRDS(H5_adj_res_BH, here("Kenya/results/adjusted/H5_res.RDS"))
